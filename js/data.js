7a0e697ced96d12da6d88a9c4336290d110b48ab
bytemaster-design <design1.media.pro@gmail.com>
2026-03-22 15:05:01 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 // данные о категориях
 const categories = [
-    { id: 'previews', name: 'Превью для видео', folder: 'previews', count: 7, ratio: 'video' },
+    { id: 'previews', name: 'Превью для видео', folder: 'previews', count: 8, ratio: 'video' },
     { id: 'avatars', name: 'Аватарки', folder: 'avatars', count: 2, ratio: 'square' },
 
     // { id: 'vk', name: 'Оформление ВК', folder: 'vk', count: 0, ratio: 'square' },
